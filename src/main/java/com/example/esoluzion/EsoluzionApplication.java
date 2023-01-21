d932df6b793456ba68491b99e3b5b5e1c8692e8d
carolina.marin <cmarinhincapie1@gmail.com>
2023-01-21 00:49:36 -0500

    Se crea proyecto prueba esoluzion

---
+++ src/main/java/com/example/esoluzion/EsoluzionApplication.java
@@ -0,0 +1,13 @@
+package com.example.esoluzion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsoluzionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsoluzionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/esoluzion/EsoluzionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.esoluzion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsoluzionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
