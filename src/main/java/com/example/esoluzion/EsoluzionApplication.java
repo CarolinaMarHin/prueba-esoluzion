789260ae8c46abb6120328f420a9a1e7dc12a45e
carolina.marin <cmarinhincapie1@gmail.com>
2023-01-21 23:40:04 -0500

    Se crea endpoint getFinalPrice

--- src/main/java/com/example/esoluzion/EsoluzionApplication.java
+++ src/main/java/com/example/esoluzion/EsoluzionApplication.java
@@ -1,11 +1,15 @@
 package com.example.esoluzion;
 
+import com.example.ecommerce.controller.ECommerceController;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan({"com.example"})
 @SpringBootApplication
+//@ComponentScan(basePackageClasses = ECommerceController.class)
+//@EnableAutoConfiguration
+@ComponentScan({"com.example"})
 public class EsoluzionApplication {
 
 	public static void main(String[] args) {
